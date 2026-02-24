280bea58718838f7ee45ba6cfac003aa8c1c6a74
Sachin Parmar <sachin@Sachins-MacBook-Air.local>
2026-02-24 16:10:09 +0530

    push

--- client/src/components/LeaseForecasterForm.js
+++ client/src/components/LeaseForecasterForm.js
@@ -33,12 +33,6 @@ const BuildingIcon = () => (
   </svg>
 );
 
-const BriefcaseIcon = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-    <path d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z" />
-  </svg>
-);
-
 const CloudUploadIcon = () => (
   <svg width="48" height="48" viewBox="0 0 24 24" fill="currentColor" className="upload-icon">
     <path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z" />
